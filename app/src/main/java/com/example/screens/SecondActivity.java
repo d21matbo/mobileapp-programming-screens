2536e60588f9ad9d5c7aeef2baa7fe91ed68c761
Mattias Borgenstierna <d21matbo@student.his.se>
2022-04-20 16:32:57 +0200

    Add code to show data from intent

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,12 +3,20 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        final String text = getIntent().getExtras().getString("NAME");
+
+        textView = findViewById(R.id.textView2);
+        textView.setText(text);
     }
 }
